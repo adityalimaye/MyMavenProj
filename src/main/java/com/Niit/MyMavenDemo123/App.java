bf02f9534fd21cf12da5c5139c453c844d7c9fbc
adityalimaye <adityalimaye777@gmail.com>
2016-08-05 17:40:02 +0530

    second week commit

---
+++ src/main/java/com/Niit/MyMavenDemo123/App.java
@@ -0,0 +1,13 @@
+package com.Niit.MyMavenDemo123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
